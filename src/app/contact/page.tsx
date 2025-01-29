6002c70ea07b7abe16b44d055206e0548897f5c6
Kelvin Sundli <kelvin.sundli@gmail.com>
2025-01-29 14:25:09 +0100

    simplify contact details page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,6 @@ export default function Contact() {
     <main className="h-full flex flex-col items-center justify-center space-y-8">
       <div className="container max-w-3xl py-10">
         <h1 className="text-4xl font-bold tracking-tight">Get in touch</h1>
-        <p className="text-muted-foreground">You can reach us at:</p>
         <ul className="list-disc list-inside">
           <li>Email: kelvin[at]sundli.ai</li>
           <li>Phone: +47 993 44 356</li>
