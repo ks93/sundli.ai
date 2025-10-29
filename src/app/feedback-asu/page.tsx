33cbacca5c029d12e6d1aa1776904b1da5cd494f
Kelvin  Sundli <kelvin.sundli@cognite.com>
2025-10-28 17:09:52 -0700

    Add redirect for /feedback-asu to Google Forms

---
+++ src/app/feedback-asu/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation'
+
+export default function FeedbackASU() {
+  redirect('https://docs.google.com/forms/d/e/1FAIpQLSfNTPyKdZmgmPqe5YxAOH6yAOG63NUMIRLnWwqOgofy5YWO4A/viewform?usp=header')
+}
+
